bd36b35998dd2ca9c4d5ad99ac853c1329a1a252
Ghelder <Jhelder@outlook.com>
2023-02-22 18:28:21 -0800

    0->Task: Positive anything is better than negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main -> init
-*
-* Return: 0 (Success)
-*/
+ * main -> init
+ *
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
@@ -18,16 +18,13 @@ int main(void)
 	{
 		printf("%d, is positive\n", n);
 	}
+	else if (n == 0)
+	{
+		printf("%d, is zero\n", n);
+	}
 	else
 	{
-		if (n < 0)
-		{
-			printf("%d, is negative\n", n);
-		}
-		else
-		{
-			printf("%d, is zero\n", n);
-		}
+		printf("%d, is negative\n", n);
 	}
 	return (0);
 }
