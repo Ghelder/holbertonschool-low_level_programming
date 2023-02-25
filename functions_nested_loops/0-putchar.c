bcccdd6209eff40400b4d5d1b5194b1cbda5b476
Ghelder <Jhelder@outlook.com>
2023-02-25 05:12:10 -0800

    0->Task: _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main -> print the word putchar
+*
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	char str[] = "_putchar";
+
+	for (i = 0; i < str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
