8f483de628baf9c5d78b405cf220a697660e66af
Ghelder <Jhelder@outlook.com>
2023-02-26 04:53:35 -0800

    2->Task: Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul -> funtion
+ * @a: parameter int
+ * @b: parameter int
+ * Multiplies two integers
+ * Return: a and b (Success)
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
