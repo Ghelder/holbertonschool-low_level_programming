6aa0cb8baa3f37c97210f2ab89e96e262199dbe1
Ghelder <Jhelder@outlook.com>
2023-02-26 10:10:58 -0800

    7->Task: I feel like I am diagonally parked in a parallel universe

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal -> function
+ * @n: is the number of times the character
+ * Return: prints the character
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
