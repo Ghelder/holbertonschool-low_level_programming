1684d671fc63c8342a2e077631d0a65ab4b1e38e
Ghelder <Jhelder@outlook.com>
2023-02-25 22:51:46 -0800

    10->Task: a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add -> function
+ * @a: parameter int
+ * @b: parameter int
+ * add two integers and return the result
+ * Return: a + b int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
