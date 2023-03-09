9aacf89fa665abd93956f82a197bd7cfdf029b5e
Ghelder <Jhelder@outlook.com>
2023-03-09 05:26:02 -0800

    1->Task: memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memcpy -> function
+ * @dest: character string one
+ * @src: character string two
+ * @n: n bytes
+ * Return: dest (Success)
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest++) = *(src++);
+	}
+	return (dest);
+}
