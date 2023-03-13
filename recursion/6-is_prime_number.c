033510ed23c51cb19bdee5c003f4eefb8bfd2f1e
Ghelder <Jhelder@outlook.com>
2023-03-12 20:12:07 -0700

    6->Task: Inception. Is it possible?

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _is_prime -> function 
+ * @a: parameter int
+ * @b: parameter int
+ * Return: _is_prime
+ */
+int _is_prime(int a, int b)
+{
+	if (a == b)
+		return (1);
+	if (a % b == 0)
+		return (0);
+	return (_is_prime(a, b + 1));
+}
+
+/**
+ * is_prime_number -> function
+ * @n: parameter int
+ * Return: _is_prime
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (_is_prime(n, 2));
+}
+
+
