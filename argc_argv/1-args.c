f9d8c6fb61ed23315ba8c89e78e369d81636390e
Ghelder <Jhelder@outlook.com>
2023-03-14 02:41:27 -0700

    1->Task: Silence is argument carried out by other means

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main ->  function
+ * @argc: parameter int
+ * @argv: parameter char
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
