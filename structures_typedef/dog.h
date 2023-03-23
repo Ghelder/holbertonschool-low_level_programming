b1315804221dc9890690e5f1f701cb604a3b1e1c
Ghelder <Jhelder@outlook.com>
2023-03-22 23:09:57 -0700

    0->Task: Defines a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -> data dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: Defines a new struct dog type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
