fbedcd52cee7de40231e42d8869da082fcc39568
Ghelder <Jhelder@outlook.com>
2023-03-23 02:51:24 -0700

    2->Task: change a NULL

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,11 +8,11 @@
 
 void print_dog(struct dog *d)
 {
-	if (!d->name)
+	if (d->name == NULL)
 		d->name = "(nill)";
-	if (!d->owner)
+	if (d->owner == NULL)
 		d->owner = "(nill)";
-	if (!d)
+	if (d == NULL)
 		return;
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
