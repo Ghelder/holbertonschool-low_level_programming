ce93cf691b9f5b8b3c8fafe5c98b56c00e6be7b1
Ghelder <Jhelder@outlook.com>
2023-03-23 03:14:01 -0700

    2->Task: Solved NULL conditions

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,11 +8,13 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
-		d->name = "(nill)";
-	if (d->owner == NULL)
-		d->owner = "(nill)";
 	if (d == NULL)
 		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
